10e7914d3e4faa8f1783fb7df416702390edbffc
W4ssim23 <o_zouiten@estin.dz>
2024-08-25 19:05:34 +0100

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { AppProvider } from "./providers";
+import { AppProvider } from "./Providers";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
